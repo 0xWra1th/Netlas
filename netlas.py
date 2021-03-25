fb860f19801dd43166b3c9cd15dcd897d14556d0
Daniel Scragg <danielscragg@outlook.com>
2021-03-25 23:15:54 +0200

    Tested GeoIP
    Currently unsuccessful.

--- netlas.py
+++ netlas.py
@@ -3,6 +3,49 @@
 #Date: 24/03/2021
 import psutil as ps
 import time, requests, json
+from geoip import geolite2
+
+def main():
+	results = ps.net_connections()
+	count = 0
+	while (True):
+		new = ps.net_connections()
+		if results != new:
+			results = new
+		for ip in results:
+			if ip[4] != () and ip[4][0] != "::1" and ip[4][0] != "127.0.0.1":
+				count+=1
+				curr = str(ip[4][0])
+				time.sleep(2)
+
+				# USING API
+				response = requests.get("http://ip-api.com/json/"+str(curr)+"?fields=status,country,city,query")
+				if 'json' in response.headers.get('Content-Type'):
+					details = response.json()
+					if details["status"] == "success":
+						if details["country"] == "South Africa":
+							print(f"{bcolors.YELLOW}"+details["country"]+"("+details["city"]+"): "+str(curr))
+						elif details["country"] == "United States": 
+							print(f"{bcolors.OKCYAN}"+details["country"]+"("+details["city"]+"): "+str(curr))
+						elif details["country"] == "France":
+							print(f"{bcolors.OKGREEN}"+details["country"]+"("+details["city"]+"): "+str(curr))
+						else: 
+							print(f"{bcolors.RED}"+details["country"]+"("+details["city"]+"): "+str(curr))
+
+				#USING GEOIP
+				#match = geolite2.lookup(b'10.22.43.222')
+				#if match is not None:
+					#country = match.country
+					#if country == "South Africa":
+						#print(f"{bcolors.YELLOW}"+country+": "+str(curr)+"\n")
+					#elif country == "United States": 
+						#print(f"{bcolors.OKCYAN}"+country+": "+str(curr)+"\n")
+					#elif country == "France":
+						#print(f"{bcolors.OKGREEN}"+country+": "+str(curr)+"\n")
+					#else: 
+						#print(f"{bcolors.RED}"+country+": "+str(curr)+"\n")
+
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,26 +61,5 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-results = ps.net_connections()
-count = 0
-while (True):
-	new = ps.net_connections()
-	if results != new:
-		results = new
-	for ip in results:
-		if ip[4] != () and ip[4][0] != "::1" and ip[4][0] != "127.0.0.1":
-			count+=1
-			curr = ip[4][0]
-			time.sleep(2)
-			response = requests.get("http://ip-api.com/json/"+str(curr)+"?fields=status,country,city,query")
-			if 'json' in response.headers.get('Content-Type'):
-				details = response.json()
-				if details["status"] == "success":
-					if details["country"] == "South Africa":
-						print(f"{bcolors.YELLOW}"+str(count)+") "+details["country"]+"("+details["city"]+"): "+str(curr)+"\n")
-					elif details["country"] == "United States": 
-						print(f"{bcolors.OKCYAN}"+str(count)+") "+details["country"]+"("+details["city"]+"): "+str(curr)+"\n")
-					elif details["country"] == "France":
-						print(f"{bcolors.OKGREEN}"+str(count)+") "+details["country"]+"("+details["city"]+"): "+str(curr)+"\n")
-					else: 
-						print(f"{bcolors.RED}"+str(count)+") "+details["country"]+"("+details["city"]+"): "+str(curr)+"\n")
+if __name__ == "__main__":
+    main()
