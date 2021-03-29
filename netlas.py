a6a89cfd6aba5d8e0bccfa05ea06b3a580ee1abb
Daniel Scragg <danielscragg@outlook.com>
2021-03-29 09:30:29 +0200

    Array and More Countries
    - Added 7 more countries
    - Implemented an array of country labels

--- netlas.py
+++ netlas.py
@@ -32,44 +32,59 @@ def main():
 	label = QLabel(window)
 	label.setPixmap(QPixmap('assets/world_map_1080_Dark.jpg'))
 
+	#Countries
 	ZA = QLabel(window)
 	ZA.setPixmap(QPixmap('assets/ZA.png'))
-	ZA.setVisible(False)
-	#ZA.move(0,0)
-	#ZA.resize(1080,536)
-	#ZA.setStyleSheet("border: 3px solid white;border-radius: 10px;background-color: white")
+	ZA.setVisible(True)
 
 	US = QLabel(window)
 	US.setPixmap(QPixmap('assets/US.png'))
-	US.setVisible(False)
-	#US.move(120,145)
-	#US.resize(30,30)
-	#US.setStyleSheet("border: 3px solid white;border-radius: 15px;background-color: white")
+	US.setVisible(True)
 
 	UK = QLabel(window)
 	UK.setPixmap(QPixmap('assets/UK.png'))
-	UK.setVisible(False)
-	#UK.move(435,100)
-	#UK.resize(10,10)
-	#UK.setStyleSheet("border: 3px solid white;border-radius: 5px;background-color: white")
+	UK.setVisible(True)
 
 	FR = QLabel(window)
 	FR.setPixmap(QPixmap('assets/FR.png'))
-	FR.setVisible(False)
-	#FR.move(445,120)
-	#FR.resize(15,15)
-	#FR.setStyleSheet("border: 3px solid white;border-radius: 7px;background-color: white")
+	FR.setVisible(True)
+
+	AU = QLabel(window)
+	AU.setPixmap(QPixmap('assets/AU.png'))
+	AU.setVisible(True)
+
+	BR = QLabel(window)
+	BR.setPixmap(QPixmap('assets/BR.png'))
+	BR.setVisible(True)
+
+	CA = QLabel(window)
+	CA.setPixmap(QPixmap('assets/CA.png'))
+	CA.setVisible(True)
+
+	CN = QLabel(window)
+	CN.setPixmap(QPixmap('assets/CN.png'))
+	CN.setVisible(True)
+
+	IN = QLabel(window)
+	IN.setPixmap(QPixmap('assets/IN.png'))
+	IN.setVisible(True)
+
+	RU = QLabel(window)
+	RU.setPixmap(QPixmap('assets/RU.png'))
+	RU.setVisible(True)
+
+	countries = [ZA, US, UK, FR, AU, BR, CA, CN, IN, RU]
 
 
 	window.show()
 
 	#Threading
-	loop = threading.Thread(target=netCheck, args=(text_area,window,ZA,US,UK,FR,))
+	loop = threading.Thread(target=netCheck, args=(text_area,window,countries,))
 	loop.start();
 
 	sys.exit(app.exec_())
 
-def netCheck(ta, win, ZA, US, UK, FR):
+def netCheck(ta, win, countries):
 	results = ps.net_connections()
 	count = 0
 	scrollbar = ta.verticalScrollBar();
@@ -83,13 +98,11 @@ def netCheck(ta, win, ZA, US, UK, FR):
 			if ip[4] != () and ip[4][0] != "::1" and ip[4][0] != "127.0.0.1":
 				count+=1
 				curr = str(ip[4][0])
-				time.sleep(2)
+				time.sleep(1)
 
 				#RESET MARKERS
-				ZA.setVisible(False)
-				US.setVisible(False)
-				UK.setVisible(False)
-				FR.setVisible(False)
+				for c in countries:
+					c.setVisible(False)
 
 				#USING API
 				response = requests.get("http://ip-api.com/json/"+str(curr)+"?fields=status,country,city,query")
@@ -98,19 +111,43 @@ def netCheck(ta, win, ZA, US, UK, FR):
 					if details["status"] == "success":
 						if details["country"] == "South Africa":
 							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
-							ZA.setVisible(True)
+							countries[0].setVisible(True)
 							ta.append(text)
 						elif details["country"] == "United States": 
 							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
-							US.setVisible(True)
-							ta.append(text)
-						elif details["country"] == "France":
-							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
-							FR.setVisible(True)
+							countries[1].setVisible(True)
 							ta.append(text)
 						elif details["country"] == "United Kingdom":
 							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
-							UK.setVisible(True)
+							countries[2].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "France":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[3].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "Australia":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[4].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "Brazil":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[5].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "Canada":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[6].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "China":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[7].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "India":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[8].setVisible(True)
+							ta.append(text)
+						elif details["country"] == "Russia":
+							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
+							countries[9].setVisible(True)
 							ta.append(text)
 						else:
 							text = "<span style=\" font-size:11pt; font-weight:200; color:white;\" >"+details["country"]+", "+details["city"]+" - "+str(curr)+" : "+str(ip[4][1])+"</span>"
